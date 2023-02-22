41fc304b4b63f0ca79ae4f3c833e974054ef89d8
Ziedgharbi <117041818+Ziedgharbi@users.noreply.github.com>
2023-02-22 19:36:39 +0100

    Add files via upload
    This is a basic example of machine learning deployment using Streamlit : results may be improved  
    
    app.py is for model training.
    annexe_app.py is for Streamlit application

---
+++ annexe_app.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 11:11:48 2023
+This script must be run at least one time before runing app.py for streamlit
+
+This script is for training models and save them
+
+@author: Zied
+"""
+from numba import jit, cuda
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+from sklearn.linear_model import LinearRegression, Ridge, SGDRegressor
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from sklearn.metrics import r2_score 
+from joblib import dump, load
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import os 
+import re 
+
+
+def set_path():
+    
+    path=os.getcwd()
+    #path='C:/Users/pc/Nextcloud/Python/streamlit_ex'
+    os.chdir(path)
+    model_path=path+"/models/"
+
+    isExist = os.path.exists(model_path)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(model_path)
+    
+    return path, model_path
+        
+    
+
+
+#@jit(target_backend='cuda')     
+def load_data():
+    dataset= load_diabetes(as_frame=True, scaled=False)
+    return(dataset)
+
+
+#@jit(target_backend='cuda')     
+def save_descreption(data):
+    """ cette fonction permet d'ajouter une descreption 
+    pour la variable sex et enregistrer 
+    la descreption dans un fichier txt """
+
+    text=str(data)
+    pattern="      - sex\n"
+    new_text=re.sub(pattern, r'      - sex     2 female, 1 male\n',text)
+    
+    # enregistrer la descreption de la base de données pour l'afficher à l'utilisateur par la suite 
+    file=open("descreption.txt", "w")
+    file.write ( new_text)
+    file.close()
+    return(new_text)
+    
+def split (X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    
+    x_mean=X_train.loc[:,X_train.columns!='sex'].mean(axis=0)
+    x_std =X_train.loc[:,X_train.columns!='sex'].std(axis=0)
+    
+    X_scaled= (X_train.loc[:,X_train.columns!='sex'] - x_mean)/x_std
+    X_scaled.insert(0,'sex',X_train['sex'])
+       
+    y_mean=y_train.mean()
+    y_std =y_train.std()
+    
+    y_scaled=(y_train-y_mean)/y_std
+    
+    
+    dict_stat={"x_mean": x_mean,
+               'x_std':x_std,
+               "y_mean":y_mean,
+               "y_std":y_std}
+    
+    return(X_scaled, X_test, y_scaled, y_test, dict_stat)
+
+
+def affichage (x):
+    fig = plt.figure(figsize = (8,10))
+    ax = fig.gca()
+    x.hist(ax=ax, color = "skyblue")
+    plt.show()
+    
+    
+def evaluate(y_test, y_pred):
+    mean_abs=mean_absolute_error(y_test, y_pred)
+    mean_squ=mean_squared_error(y_test, y_pred)
+    r2=r2_score(y_test, y_pred)
+    return([mean_abs,mean_squ,r2])
+    
+    
+
+
+@jit(target_backend='cuda')
+def train(models,params,X,y,name,model_path):
+    
+    X_scaled, X_test, y_scaled, y_test, dict_stat=split(X,y)
+    
+    x_mean=dict_stat["x_mean"]
+    x_std=dict_stat["x_std"]
+    y_mean=dict_stat["y_mean"]
+    y_std=dict_stat["y_std"]
+    
+    
+    rslt=[]
+    i=0
+
+ 
+   
+    for i in range(3):
+        model=models[i]
+        param=params[i]
+        clf=GridSearchCV(model, param)
+        clf.fit(X_scaled,y_scaled)
+        
+        
+        X_test_scaled= (X_test.loc[:,X_test.columns!='sex'] - x_mean)/x_std
+        X_test_scaled.insert(0,'sex',X_test['sex'])
+        
+        y_pred=clf.predict(X_test_scaled)
+        #clf.best_estimator_
+        #clf.best_params_
+        dump(clf.best_estimator_, model_path+name[i ]+".joblib") 
+        
+        y_std_array=np.full((y_pred.shape[0],1), y_std)
+        y_mean_array=np.full((y_pred.shape[0],1),y_mean)
+
+
+        
+        y_pred=np.ravel(y_pred)* np.ravel(y_std_array )+np.ravel(y_mean_array)
+        score=evaluate (np.ravel(y_test),y_pred)
+        rslt.append(  {"model" : name[i],
+                       "mean_abs": score[0],
+                       "mean_squ": score[1],
+                       "r2":score[2]})
+    return(rslt)
+    
+    
+def main():
+   
+    path,model_path=set_path()
+    
+    data=load_data()
+    print(save_descreption(data['DESCR']))
+ 
+
+    X=data["data"]
+    y=data["target"].to_frame()
+    
+    y.describe()
+    
+    for col in list(X.columns):
+        print(X[col].describe())
+    
+    name_x=list(X.columns)
+    name_y=list(y.columns)  
+  
+  
+    
+    
+   
+    affichage(X.loc[:,X.columns!='sex'])
+    
+ 
+    model1=LinearRegression()
+    param1={"fit_intercept": [False, True],
+            "positive": [False, True]}   
+    model2=Ridge()
+    param2={"alpha": [1,10,20,30],
+            "fit_intercept" :[False, True] ,
+            
+            "solver" : ["svd", "cholesky", "lsqr", "sparse_cg", "sag", "saga", "lbfgs"]} 
+    model3=SGDRegressor()
+    param3={"loss": ["squared_error","huber"],
+            "penalty": ["ll1","l2","elasticnet"],
+            "alpha": [0.1,1,10,0.2,0.3],
+            "fit_intercept":[False,True]}
+    
+    models=[model1,model2,model3]
+    params=[param1,param2,param3]
+    name=["Linear_models", "Ridge_model","SGDR_model"]
+    end_score= train(models,params,X,y,name,model_path)
+    
+    
+    
+    l=list( end_score[1].keys())
+   
+    results=pd.DataFrame([],index=name, columns= l[-3:])
+    
+    for i in range(3):
+        ll=[]
+        dic=end_score[i]
+        for key, values in dic.items():
+            if type(values)!=str:
+                ll.append(values)
+        results.iloc[i,:]=ll
+    
+    results.to_csv(path+"/rslt.txt", header=True, index=True, sep=',', mode='w')
+    
+    return(results)
+
+
+
+main()
+
---
+++ app.py
@@ -0,0 +1,156 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from joblib import dump, load
+from sklearn.linear_model import LinearRegression, Ridge, SGDRegressor
+
+
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+from sklearn.linear_model import LinearRegression, Ridge, SGDRegressor
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from sklearn.metrics import r2_score 
+from joblib import dump, load
+
+from numba import jit, cuda
+
+import os 
+import re 
+
+from annexe_app import *
+
+
+
+def main():
+    
+    
+    path, model_path=set_path()
+    
+    
+    data=load_data()
+    X=data["data"]
+    y=data["target"].to_frame()
+    
+    X_train, X_test, y_train, y_test, dict_stat=split(X,y)
+    
+    x_mean=dict_stat["x_mean"]
+    x_std=dict_stat["x_std"]
+    y_mean=dict_stat["y_mean"]
+    y_std=dict_stat["y_std"]
+    
+    
+    st.title("Diabet disease progression one year prediction, exemple")
+    st.subheader("By Zied GHARBI")
+    f=open(path+"/descreption.txt", "r")
+    desc=f.read()
+    f.close()
+    st.markdown( """ 
+    <br>  <br/>
+    This application include 3 classics models for prediction namelly : simple linear regression, Ridge regression and Linear model fitted by minimizing a regularized empirical loss with SGD.
+    These models were trained on data set descrebied as folow :
+    """, unsafe_allow_html=True
+    )
+    
+    st.text(desc)
+    
+    st.sidebar.markdown('# Choose your parameters')
+    
+    age=st.sidebar.number_input('Age of patient: ',min_value=1,max_value=100,step=1)
+    
+    sex = st.sidebar.selectbox("Sex of patient:", ("Male","Female"))
+    
+    bmi=st.sidebar.number_input("Bmi value :", min_value=16.00, max_value=100.00, step=0.01)
+    
+    st.sidebar.text("")
+    bp=st.sidebar.number_input('Bp index :',min_value=60.00, max_value=133.00,step=0.01)
+    
+    tc=st.sidebar.number_input('Tc value :',min_value=90.00, max_value=310.00,step=0.01)
+    
+    ldl=st.sidebar.number_input('Ldl value :',min_value=39.00, max_value=245.00,step=0.01)
+    
+    hdl=st.sidebar.number_input('Hdl value :',min_value=19.00, max_value=105.00,step=0.01)
+    
+    tch=st.sidebar.number_input('Tch value :',min_value=0.00, max_value=15.00,step=0.01)
+    
+    ltg=st.sidebar.number_input('Ltg value :',min_value=0.00, max_value=10.00,step=0.01)
+    
+    glu=st.sidebar.number_input('Glu value :',min_value=50.00, max_value=124.00,step=0.01)
+    
+    st.sidebar.markdown('# Choose your model(s) for prediction :')
+    model=st.sidebar.multiselect("Models : multiselect authorised :", options=["Linear regression","Ridge regression","SGD regression" ],default=["Linear regression"] )
+    
+    
+  
+    if st.sidebar.button("Calculate",key="butt"):
+        
+        rslt={}
+        
+        if sex=='Male':
+            sex=1
+        else :
+            sex=2
+    
+        for model in model :
+            if model=="Linear regression":
+                lr=load(model_path+'/Linear_models.joblib')
+
+                x=np.float_([sex,age,bmi,bp,tc,ldl,hdl,tch,ltg,glu])
+                x=x.reshape(1,-1)
+                x=pd.DataFrame(x,columns=['sex', 'age', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6'])
+
+                data = (x.loc[:,x.columns!='sex'] - x_mean)/x_std
+
+                data.insert(0,"sex",x["sex"])
+
+                data=data.to_numpy(dtype=float)
+
+                rs=lr.predict(data)
+                rslt["Linear regression"]=float(rs)*y_std+y_mean
+                
+            if model=="Ridge regression":
+                lr=load(model_path+'/Ridge_model.joblib')
+                x=np.float_([sex,age,bmi,bp,tc,ldl,hdl,tch,ltg,glu])
+                x=x.reshape(1,-1)
+                x=pd.DataFrame(x,columns=['sex', 'age', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6'])
+
+                data = (x.loc[:,x.columns!='sex'] - x_mean)/x_std
+
+                data.insert(0,"sex",x["sex"])
+
+                data=data.to_numpy(dtype=float)
+
+                rs=lr.predict(data)
+                rslt["Ridge regression"]=float(rs)*y_std+y_mean
+            
+            if model=="SGD regression":
+                lr=load(model_path+'/SGDR_model.joblib')
+                x=np.float_([sex,age,bmi,bp,tc,ldl,hdl,tch,ltg,glu])
+                x=x.reshape(1,-1)
+                x=pd.DataFrame(x,columns=['sex', 'age', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6'])
+
+                data = (x.loc[:,x.columns!='sex'] - x_mean)/x_std
+
+                data.insert(0,"sex",x["sex"])
+
+                data=data.to_numpy(dtype=float)
+
+                rs=lr.predict(data)
+                rslt["SGD regression"]=float(rs)*y_std+y_mean
+        text=""
+        
+        for key , value in rslt.items():
+            text=text+" \n "+ str(key)+" ---- "+str(value)
+        
+        st.text(text)
+            
+    
+     
+    
+
+
+
+if __name__ == '__main__':
+
+    main()
